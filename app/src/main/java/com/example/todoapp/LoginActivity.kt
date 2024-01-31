92e06c9bcd87f274ce8858ecc494bca46cd18649
Iyarr <8.24ridiculous@gmail.com>
2024-01-31 11:43:26 +0900

    READMEの修正と使用するライブラリの修正

--- app/src/main/java/com/example/todoapp/LoginActivity.kt
+++ app/src/main/java/com/example/todoapp/LoginActivity.kt
@@ -2,10 +2,13 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
 }
